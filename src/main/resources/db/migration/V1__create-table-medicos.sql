fac5d5073a50f1825ebdb92083b67cada4a12cab
FernandezOscar96 <oskyfernandezz@gmail.com>
2023-06-02 19:59:32 -0300

    modificaciones de table medico/paciente

--- src/main/resources/db/migration/V1__create-table-medicos.sql
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -3,6 +3,7 @@ create table medicos(
     nombre varchar(100) not null,
     email varchar(100) not null unique,
     documento varchar(6) not null unique,
+    telefono varchar(20) not null,
     especialidad varchar(100) not null,
     calle varchar(100) not null,
     distrito varchar(100) not null,
