fe6b0d190e898a4db4fec725590b85b72de2ac3d
FernandezOscar96 <oskyfernandezz@gmail.com>
2023-06-06 21:36:23 -0300

    add respuesta de 201

--- src/main/java/meed/voll/api/controller/MedicoController.java
+++ src/main/java/meed/voll/api/controller/MedicoController.java
@@ -2,13 +2,17 @@ package meed.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import meed.voll.api.direction.DirectionData;
 import meed.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,9 +22,23 @@ public class MedicoController {
     private MedicoRepository medicoRepository;
 
     @PostMapping
-    public void registrarMedico(@RequestBody @Valid MedicDataReg MedicDataReg) {
+    public ResponseEntity<RequestMedicData> registrarMedico(@RequestBody @Valid MedicDataReg MedicDataReg, UriComponentsBuilder uriComponentsBuilder) {
+        Medico medico = medicoRepository.save(new Medico(MedicDataReg));
+        RequestMedicData requestMedicData = new RequestMedicData(
+                medico.getId(),
+                medico.getNombre(),
+                medico.getEmail(),
+                medico.getTelefono(),
+                medico.getEspecialidad().toString(),
+                new DirectionData(
+                        medico.getDireccion().getCalle(),
+                        medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),
+                        medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento()));
 
-        medicoRepository.save(new Medico(MedicDataReg));
+        URI url = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+        return ResponseEntity.created(url).body(requestMedicData);
     }
 
     @GetMapping
@@ -32,18 +50,30 @@ public class MedicoController {
 
     @PutMapping
     @Transactional
-    public void actualizarMedico(@RequestBody @Valid MedicActData medicActData) {
+    public ResponseEntity actualizarMedico(@RequestBody @Valid MedicActData medicActData) {
         Medico medico = medicoRepository.getReferenceById(medicActData.id());
         medico.actualizarDatos(medicActData);
+        return ResponseEntity.ok(new RequestMedicData(
+                medico.getId(),
+                medico.getNombre(),
+                medico.getEmail(),
+                medico.getTelefono(),
+                medico.getEspecialidad().toString(),
+                new DirectionData(
+                        medico.getDireccion().getCalle(),
+                        medico.getDireccion().getDistrito(),
+                        medico.getDireccion().getCiudad(),
+                        medico.getDireccion().getNumero(),
+                        medico.getDireccion().getComplemento())));
     }
 
     //DELETE logico
     @DeleteMapping("/eliminarMedico/{id}")
     @Transactional
-    public void eliminarMedico(@PathVariable Long id) {
+    public ResponseEntity eliminarMedico(@PathVariable Long id) {
         Medico medico = medicoRepository.getReferenceById(id);
         medico.desactivarMedico();
-
+        return ResponseEntity.noContent().build();
     }
 
 
