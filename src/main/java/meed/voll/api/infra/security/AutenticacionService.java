dda57d228e28aca0e111d80fc545ee6eda1fe4f7
FernandezOscar96 <oskyfernandezz@gmail.com>
2023-06-09 21:15:34 -0300

    adds new class security

---
+++ src/main/java/meed/voll/api/infra/security/AutenticacionService.java
@@ -0,0 +1,18 @@
+package meed.voll.api.infra.security;
+
+import meed.voll.api.domain.usuarios.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AutenticacionService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByLogin(username);
+    }
+}
